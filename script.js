17fd8c3ea0801c0e0a8fa8ed714079ddb14ac8e0
rhu-machado <rhu.machado16@gmail.com>
2022-06-30 14:44:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let me = {
+    nome: "Rhuan",
+    idade: 22,
+    altura: 1.81
+}
+
+me.olhos = "castanhos"
+
+delete me.idade
+
+console.log(me)
+
+
+
+let cadastro = [
+    {
+        nome: 'Evelyn',
+        idade: 22,
+        telefone: 21987654321,
+        amigos: ['Isabele', 'Clarissa']
+    },{
+        nome: 'Enzo',
+        idade: 7,
+        telefone: 21977663322,
+        amigos: ['Miguel', 'Gabriel']
+    },{
+        nome: 'Daniel',
+        idade: 20,
+        telefone: 21997654321,
+        amigos: ['Rhaun', 'Bruno']
+    },{
+        nome: 'Bruno',
+        idade: 20,
+        telefone: 21967654321,
+        amigos: ['Daniel', 'Nirvana']
+    },{
+        nome: 'Rhuan',
+        idade: 22,
+        telefone: 2199764794,
+        amigos: ['Bruno', 'Evelyn']
+    }]
+
+    console.log(cadastro[4].amigos[1])
